4993326e7de927473b5fcb8f43def9386c122588
Alexssander Castro <147998833+AlexssanderCastro@users.noreply.github.com>
2025-08-04 20:43:31 -0300

    Add files via upload

---
+++ Aula1/PrjClienteSockets/src/main/java/com/mycompany/prjclientesockets/PrjClienteSockets.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prjclientesockets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author Alexssander
+ */
+public class PrjClienteSockets {
+
+    public static void main(String[] args)
+    {
+        try{
+            Socket con = new Socket("127.0.0.1",2222);
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            PrintStream saida = new PrintStream(con.getOutputStream());
+            String linha;
+            BufferedReader entradaTeclado = new BufferedReader(new InputStreamReader(System.in));
+            while(true){
+                System.out.println("> ");
+                linha=entradaTeclado.readLine();
+                saida.println(linha);
+                linha=entrada.readLine();
+                if(linha==null){
+                    System.out.println("Conexao encerrada!!!");
+                    break;
+                }
+                System.out.println(linha);
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+        
+    }
+}
